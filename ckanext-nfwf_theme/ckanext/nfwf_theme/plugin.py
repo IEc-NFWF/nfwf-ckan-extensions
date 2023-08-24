8c61350b745cd5a63ee940198e353925b91bac24
jimanderton <janderton@indecon.com>
2023-08-24 14:51:03 -0700

    wip - Modifications for updated extension requirements

--- ckanext-nfwf_theme/ckanext/nfwf_theme/plugin.py
+++ ckanext-nfwf_theme/ckanext/nfwf_theme/plugin.py
@@ -11,3 +11,4 @@ class Nfwf_ThemePlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'nfwf_theme')
+        toolkit.add_resource('assets', 'nfwf_assets')
