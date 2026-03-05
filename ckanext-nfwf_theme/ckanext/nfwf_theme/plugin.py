e1ce637e7792d07f59af1ae13d8e6ec8c91c4d5b
Janusz Strzepek <jstrzepek@indecon.com>
2026-03-05 15:33:08 -0700

    Task 105. Revise Homepage. Also required updates to search indexing

--- ckanext-nfwf_theme/ckanext/nfwf_theme/plugin.py
+++ ckanext-nfwf_theme/ckanext/nfwf_theme/plugin.py
@@ -1,6 +1,6 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-
+import ckanext.nfwf_fields.plugin as nfwf_fields_plugin
 
 def most_popular_groups():
     '''Return a sorted list of the groups with the most datasets.'''
@@ -17,12 +17,38 @@ def most_popular_groups():
 def get_site_statistics():
     stats = {}
     stats['dataset_count'] = toolkit.get_action('package_search')(
-        {}, {"rows": 1})['count']
+        {}, {
+            "rows": 1,
+            "include_private": True
+        })['count']
     stats['group_count'] = len(toolkit.get_action('group_list')({}, {}))
     stats['organization_count'] = len(
         toolkit.get_action('organization_list')({}, {}))
     return stats
 
+def get_nbs_statistics():
+    stats = {}
+    nature_based_list = nfwf_fields_plugin.nature_based_solutions()
+    for nbs in nature_based_list:
+        nbs_count = toolkit.get_action('package_search')(
+            {}, {
+                "rows": 1,
+                "fq": f'nature_based:"{nbs}"',
+                "include_private": True,
+            })['count']
+        stats[nbs] = nbs_count
+    return stats
+
+def mid_break(text):
+    """ Add a line break at the space closest to the middle
+    of the text, if there is one. """
+
+    mid = len(text) // 2
+    spaces = [i for i, c in enumerate(text) if c == ' ']
+    if not spaces:
+        return text
+    closest = min(spaces, key=lambda i: abs(i - mid))
+    return text[:closest] + '<br>' + text[closest + 1:]
 
 class Nfwf_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
@@ -44,5 +70,7 @@ class Nfwf_ThemePlugin(plugins.SingletonPlugin):
         # other extensions.
         return {
             'Nfwf_Theme_most_popular_groups': most_popular_groups,
-            "Nfwf_Theme_get_site_statistics": get_site_statistics
+            "Nfwf_Theme_get_site_statistics": get_site_statistics,
+            "Nfwf_Theme_get_nbs_statistics": get_nbs_statistics,
+            "Nfwf_Theme_mid_break": mid_break,
             }
